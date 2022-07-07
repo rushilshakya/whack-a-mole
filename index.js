cdfd45dc726183d8998db14820c7af783858f7eb
rushilshakya <rushil.shakya@gmail.com>
2022-07-07 13:02:54 +0545

    feat: add random mole in hole

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 let score = 0;
+let myHoles = document.querySelectorAll(".hole");
+let myHolesArr = Array.from(myHoles);
+let newDiv = document.createElement("div");
+let outerDiv = document.querySelector("#whack-a-mole");
+newDiv.innerHTML = "hello";
+
+let previousNumber = 0;
+myHoles[previousNumber].classList.add("mole");
+
+setInterval(() => {
+  myHoles[previousNumber].classList.remove("mole");
+
+  let myRandomNumber = Math.floor(Math.random() * 9);
+  myHoles[myRandomNumber].classList.add("mole");
+  previousNumber = myRandomNumber;
+}, 1000);
+// outerDiv.appendChild(newDiv)
+// outerDiv.removeChild
